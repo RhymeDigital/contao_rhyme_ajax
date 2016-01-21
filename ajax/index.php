f93a0baa8c47430f516cf100a27de03fdddf0f1d
blairwinans <blair@rhyme.digital>
2016-01-21 09:14:33 -0500

    Figured out why TL_ASSETS_URL was not being set!

--- ajax/index.php
+++ ajax/index.php
@@ -90,9 +90,16 @@ class AjaxFrontend extends \PageRegular
 			$objPage = $this->getPageDetails($intPage);
 
 			// Define the static URL constants
-			define('TL_FILES_URL', ($objPage->staticFiles != '' && !$GLOBALS['TL_CONFIG']['debugMode']) ? $objPage->staticFiles . TL_PATH . '/' : '');
-			define('TL_SCRIPT_URL', ($objPage->staticSystem != '' && !$GLOBALS['TL_CONFIG']['debugMode']) ? $objPage->staticSystem . TL_PATH . '/' : '');
-			define('TL_PLUGINS_URL', ($objPage->staticPlugins != '' && !$GLOBALS['TL_CONFIG']['debugMode']) ? $objPage->staticPlugins . TL_PATH . '/' : '');
+			if(version_compare(VERSION, '3.5', '>='))
+			{
+    			$this->setStaticUrls();
+            }
+            else
+            {            
+    			define('TL_FILES_URL', ($objPage->staticFiles != '' && !$GLOBALS['TL_CONFIG']['debugMode']) ? $objPage->staticFiles . TL_PATH . '/' : '');
+    			define('TL_SCRIPT_URL', ($objPage->staticSystem != '' && !$GLOBALS['TL_CONFIG']['debugMode']) ? $objPage->staticSystem . TL_PATH . '/' : '');
+    			define('TL_PLUGINS_URL', ($objPage->staticPlugins != '' && !$GLOBALS['TL_CONFIG']['debugMode']) ? $objPage->staticPlugins . TL_PATH . '/' : '');
+            }
 
 			// Get the page layout
 			$objLayout = $this->getPageLayout($objPage);
